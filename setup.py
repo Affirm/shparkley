56a744a477f61344c3042e00870872259c364c8b
Niloy Gupta <niloy.gupta@affirm.com>
2020-05-20 21:25:17 -0700

    add required packages

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ import setuptools
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+REQUIRED_PACKAGES = [
+    'future~=0.16',
+    'mock~=2.0.0',
+    'pyspark>=2.4.3',
+]
+
 setuptools.setup(
     name="shparkley",
-    version="0.0.1",
+    version="0.0.2",
+    install_requires=REQUIRED_PACKAGES,
     author="niloygupta",
     author_email="niloy.gupta@affirm.com",
     description="Scaling Shapley Value computation using Spark",
