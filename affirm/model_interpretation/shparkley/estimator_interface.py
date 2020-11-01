a6d5aa6314ec7a65d2fcbc0eeab48753d19a7f8d
Isaac Joseph <Isaac.Joseph@affirm.com>
2020-10-31 19:30:21 -0700

    tests

--- affirm/model_interpretation/shparkley/estimator_interface.py
+++ affirm/model_interpretation/shparkley/estimator_interface.py
@@ -7,10 +7,43 @@ from collections import OrderedDict
 from collections.abc import MutableSet
 
 
+@add_metaclass(ABCMeta)
+class ShparkleyModel(object):
+    """
+    Abstract class for computing Shapley values.
+    """
+
+    def __init__(self, model):
+        # type: (Any) -> None
+        self._model = model
+
+    @abstractmethod
+    def get_required_features(self):
+        # type: () -> OrderedSet[str]
+        """
+        Returns an ordered set of feature names (ordered so that when recreating a
+        feature matrix from a list of per-row dicts, the columns will be in the expected order)
+        :return: OrderedSet of feature names
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self, feature_matrix):
+        # type: (List[Dict[str, Any]]) -> List[float]
+        """
+        Run the machine learning model on a feature matrix and return the predictions for each row. Take care to
+        construct the feature matrix with the same column ordering that the model was fit on.
+        :param feature_matrix: Row of feature vectors. Each entry is a dictionary mapping from the feature name to
+        feature value
+        :return: Model predictions for all feature vectors
+        """
+        raise NotImplementedError
+
+
 class OrderedSet(OrderedDict, MutableSet):
     """
-    Since use pyspark.sql.Row.asDict(), duplicate column names are not supported. However, column order must be maintained
-    for models that do not check column names explicitly during prediction.
+    Since use pyspark.sql.Row.asDict(), duplicate column names are not supported. However, column order must be
+    maintained for models that do not check column names explicitly during prediction.
     """
 
     def __init__(self, *args, **kwargs):
@@ -48,35 +81,3 @@ class OrderedSet(OrderedDict, MutableSet):
 
     def __str__(self):
         return "{%s}" % (", ".join(map(repr, self.keys())))
-
-
-@add_metaclass(ABCMeta)
-class ShparkleyModel(object):
-    """
-    Abstract class for computing Shapley values.
-    """
-
-    def __init__(self, model):
-        # type: (Any) -> None
-        self._model = model
-
-    @abstractmethod
-    def get_required_features(self):
-        # type: () -> OrderedSet[str]
-        """
-        Returns an ordered set of feature names
-        :return: OrderedSet of feature names (ordered so that when recreating a
-        feature matrix from a list of per-row dicts, the columns will be in the expected order)
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def predict(self, feature_matrix):
-        # type: (List[Dict[str, Any]]) -> List[float]
-        """
-        Run the machine learning model on a feature matrix and return the predictions for each row.
-        :param feature_matrix: Row of feature vectors. Each entry is a dictionary mapping
-        from the feature name to feature value
-        :return: Model predictions for all feature vectors
-        """
-        raise NotImplementedError
